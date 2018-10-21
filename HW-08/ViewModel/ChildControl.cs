465ad0ea6deb7d14e12ffc84176685bb3d2dc1a5
Michael Lewellen <michael.lewellen@snow.edu>
2018-10-21 14:42:51 -0600

    A little further down the road

---
+++ HW-08/ViewModel/ChildControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_08.ViewModel
+{
+    public class ChildControl
+    {
+        public ChildControl(string header, object viewModel)
+        {
+            Header = header;
+            ViewModel = viewModel;
+        }
+        public string Header { get; set; }
+        public object ViewModel { get; set; }
+    }
+}
---
+++ HW-08/ViewModel/MainViewModel.cs
@@ -0,0 +1,75 @@
+﻿using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_08.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            ChildViewModels = new ObservableCollection<ChildControl>();
+        }
+
+        private ObservableCollection<ChildControl> childViewModels;
+        public ObservableCollection<ChildControl> ChildViewModels
+        {
+            get {return childViewModels; }
+            set { SetField(ref childViewModels, value); }
+        }
+
+        private ChildControl selectedChildViewModel;
+        public ChildControl SelectedChildViewModel
+        {
+            get { return selectedChildViewModel; }
+            set { SetField(ref selectedChildViewModel, value); }
+        }
+
+        private RelayCommand treeViewDemonstration;
+        public RelayCommand TreeViewDemonstration => treeViewDemonstration ?? (treeViewDemonstration = new RelayCommand(
+            () =>
+            {
+                ChildViewModels.Add(new ChildControl("TreeView Demonstration", new TreeViewViewModel()));
+                SelectedChildViewModel = ChildViewModels.Last();
+            }));
+
+        private RelayCommand dataGridDemonstration;
+        public RelayCommand DataGridDemonstration => dataGridDemonstration ?? (dataGridDemonstration = new RelayCommand(
+            () =>
+            {
+                ChildViewModels.Add(new ChildControl("DataGrid Demonstration", new DataGridViewModel()));
+                SelectedChildViewModel = ChildViewModels.Last();
+            }));
+
+        private RelayCommand mortgageCalculatorDemonstration;
+        public RelayCommand MortgageCalculatorDemonstration => mortgageCalculatorDemonstration ?? (mortgageCalculatorDemonstration = new RelayCommand(
+            () =>
+            {
+                ChildViewModels.Add(new ChildControl("Mortgage Calculator", new MortgageCalculatorViewModel()));
+                SelectedChildViewModel = ChildViewModels.Last();
+            }));
+
+        #region INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        #endregion
+    }
+}
